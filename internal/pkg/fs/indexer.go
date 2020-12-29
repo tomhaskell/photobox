30561efe79f97c2220a63378e13add4d36f15939
Tom Haskell <tomfhaskell@gmail.com>
2020-12-29 22:10:51 +0000

    dev(indexer): refactoring to allow better future expansion
    There was no point in parrallelising the creation of the initial FileRecord as it's already reliant
    on filepath.walk, but have kept an empty processing worker for now for when I add the exif parser.
    Also moved the FileRecord creation into a factory func inside file.go, and added ability to skip
    directory/tmp file entries based on file mode

--- internal/pkg/fs/indexer.go
+++ internal/pkg/fs/indexer.go
@@ -11,38 +11,23 @@ import (
 )
 
 // Scan will search the provided path, and return a list of all the files
-func Scan(ctx context.Context, path string, concurrency int) ([]*media.File, error) {
-	files := make([]*media.File, 0)
+func Scan(ctx context.Context, path string, concurrency int) ([]*media.FileRecord, error) {
+	files := make([]*media.FileRecord, 0)
 
 	// concurrency sync
 	var wg sync.WaitGroup
+	wg.Add(concurrency)
 
 	// create pipeline streams
-	pathStream := make(chan string)
-	fileStream := make(chan *media.File)
+	fileStream := make(chan *media.FileRecord)
 
 	// setup workers
 	for x := 0; x < concurrency; x++ {
-		go makeFileWorker(ctx, &wg, pathStream, fileStream)
+		go processFileWorker(ctx, &wg, fileStream)
 	}
-	wg.Add(concurrency)
 
 	// find files
-	go func() {
-		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-			// loop over all items
-			if err == nil {
-				select {
-				case pathStream <- path:
-				case <-ctx.Done():
-					return ctx.Err()
-				}
-			}
-			return err
-		})
-		fmt.Println(err)
-		close(pathStream)
-	}()
+	go startStream(ctx, fileStream, path)
 
 	// process files
 	go func() {
@@ -52,25 +37,41 @@ func Scan(ctx context.Context, path string, concurrency int) ([]*media.File, err
 	}()
 
 	wg.Wait()
-	close(fileStream)
 
 	return files, nil
 }
 
-func makeFileWorker(ctx context.Context, wg *sync.WaitGroup, pathStream chan string, fileStream chan *media.File) {
-	defer wg.Done()
-	for path := range pathStream {
+func startStream(ctx context.Context, fileStream chan *media.FileRecord, path string) error {
+	// find all files in path and it's subdirs
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		//fmt.Printf("Processing [%v]\n", path)
-		select {
-		case fileStream <- &media.File{
-			FileName: filepath.Base(path),
-			Location: filepath.Dir(path),
-			FileType: filepath.Ext(path),
-		}:
-		case <-ctx.Done():
-			return
+		if err == nil {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+				f, err2 := media.NewFileRecordWithModeCheck(path, info)
+				if err2 == nil {
+					fileStream <- f
+				}
+			}
 		}
+		return err
+	})
+	close(fileStream)
+	return err
+}
 
+func processFileWorker(ctx context.Context, wg *sync.WaitGroup, fileStream chan *media.FileRecord) {
+	defer wg.Done()
+	for f := range fileStream {
+		fmt.Printf("Processing [%v]\n", f.FileName)
+		//select {
+		//case fileStream <- media.NewFileRecordWithModeCheck(path):
+		//case <-ctx.Done():
+		// return
 	}
+
+	// }
 	fmt.Println("ended")
 }
--- internal/pkg/media/file.go
+++ internal/pkg/media/file.go
@@ -1,7 +1,40 @@
 package media
 
-type File struct {
-	FileName string
-	Location string
-	FileType string
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// FileRecord holds information about a media file
+type FileRecord struct {
+	FileName string // the actual name of the file (incl. extension)
+	BaseName string // the base name of the file (excl. extension)
+	Location string // the dir location of the file
+	FileExt  string // the file extension
+}
+
+// NewFileRecordFromPath creates a new FileRecord from the provided `path`
+func NewFileRecordFromPath(path string) *FileRecord {
+	fileName := filepath.Base(path)
+	ext := filepath.Ext(path)
+	// trim extension
+	name := fileName[:len(fileName)-len(ext)]
+	return &FileRecord{
+		FileName: fileName,
+		BaseName: name,
+		Location: filepath.Dir(path),
+		FileExt:  ext,
+	}
+}
+
+// NewFileRecordWithModeCheck checks if the file specified is a regular file (i.e. has no mode bits
+// set for things like dir, symlink, temp file, etc.) before creating the FileRecord. If it is not a
+// regular file, an error will be returned
+func NewFileRecordWithModeCheck(path string, info os.FileInfo) (*FileRecord, error) {
+	if info.Mode().IsRegular() {
+		return NewFileRecordFromPath(path), nil
+	} else {
+		return nil, fmt.Errorf("Not a regular file")
+	}
 }
